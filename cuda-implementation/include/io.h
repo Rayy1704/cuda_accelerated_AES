385f212203f99440ecd280c026b6c59eceb4bcfd
Rayyan Salman <ryaansalman@gmail.com>
2026-04-19 21:33:33 +0500

    fix: correct extern "C" block placement in io.h

--- cuda-implementation/include/io.h
+++ cuda-implementation/include/io.h
@@ -3,8 +3,16 @@
 
 #include <stddef.h>
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 void write_buf_to_file(const unsigned char *buf, size_t len, const char *out_filename);
 unsigned char* read_file(const char* fileName, size_t *out_len);
 unsigned char * read_key_file(const char *keyFileName);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif
